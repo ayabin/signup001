b5e7b30a4f786a4fea51d8752c49a006d6b21daa
ayabin <ayabin.jp@gmail.com>
2021-02-07 13:37:41 +0900

    initial commit just before firebaseauth.

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+const String title = 'SignUp by Firebase';
+const Color kAccentColor = Color(0xFFFE7C64);
+const Color kBackgroundColor = Color(0xFF19283D);
+const Color kTextColorPrimary = Color(0xFFECEFF1);
+const Color kTextColorSecondary = Color(0xFFB0BEC5);
+const Color kButtonColorPrimary = Color(0xFFECEFF1);
+const Color kButtonTextColorPrimary = Color(0xFF455A64);
+const Color kIconColor = Color(0xFF455A64);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData.dark(),
+      routes: {
+        '/': (_) => MainPage(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: _SignInForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomeTextField extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final bool obscureText;
+  const _CustomeTextField({
+    Key key,
+    @required this.labelText,
+    @required this.hintText,
+    @required this.obscureText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        hintStyle: TextStyle(color: kTextColorSecondary),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: kAccentColor,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: kTextColorSecondary,
+          ),
+        ),
+      ),
+      obscureText: obscureText,
+      onTap: () {
+        print('TextField Tap');
+      },
+      onChanged: (String value) {
+        print(value);
+      },
+    );
+  }
+}
+
+class _SignInForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomeTextField(
+          labelText: 'Email',
+          hintText: 'your email address goes here',
+          obscureText: false,
+        ),
+        SizedBox(height: 48),
+        _CustomeTextField(
+          labelText: 'Password',
+          hintText: 'your password goes here',
+          obscureText: true,
+        ),
+        SizedBox(height: 4),
+        Text(
+          'Forgot Password?',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(height: 48),
+        Container(
+          width: double.infinity,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kButtonColorPrimary,
+              padding: EdgeInsets.symmetric(vertical: 16),
+            ),
+            child: Text(
+              'Sign up',
+              style: Theme.of(context)
+                  .textTheme
+                  .button
+                  .copyWith(color: kButtonTextColorPrimary, fontSize: 18),
+            ),
+            onPressed: () {
+              print('Sign Up');
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
